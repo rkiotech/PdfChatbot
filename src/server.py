ba08d1ae7478be1c5b4b39d4247ab4535c670368
rahul <rk>
2025-10-12 23:42:16 +0530

    mcp-1

--- src/server.py
+++ src/server.py
@@ -16,15 +16,15 @@ def special_add(a: int, b: int):
     """Add two numbers together. a and b are integers."""
     return a + b
 
-@mcp.tool
-def bring_todo_item(id: int):
-    """To fetch an item, use the bring_todo_item tool with the parameter item (integer)."""
-    response=requests.get(f"https://jsonplaceholder.typicode.com/todos/{id}")
-    if response.status_code != 200:
-        return "Item not found."
-    data = response.json()
-    item = data.get("title", "No title found.")
-    return f"Here is your to-do item: {item}"
+# @mcp.tool
+# def bring_todo_item(id: int):
+#     """To fetch an item, use the bring_todo_item tool with the parameter item (integer)."""
+#     response=requests.get(f"https://jsonplaceholder.typicode.com/todos/{id}")
+#     if response.status_code != 200:
+#         return "Item not found."
+#     data = response.json()
+#     item = data.get("title", "No title found.")
+#     return f"Here is your to-do item: {item}"
 
 if __name__ == "__main__":
     mcp.run(transport="stdio")
